9fd8b838eb5f1c1b1b3f522dc0e1caeb9ddc8a85
mwerumuchai <mwerumuchai@gmail.com>
2017-11-25 09:45:21 +0300

    create an update_profile function that will be rendered in the profile html file

--- gram/views.py
+++ gram/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib import messages
+from django.db import transaction
+from .forms import UserForm,ProfileForm
 
 
 @login_required(login_url='/accounts/login/')
@@ -13,3 +16,24 @@ def homepage(request):
 
 def logout(request):
     return render(request, 'index.html')
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, ('Your profile was successfully updated!'))
+            return redirect('home')
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
