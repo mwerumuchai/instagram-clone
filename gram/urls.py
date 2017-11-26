258f104be772d370f53a9fe40251a56f3a571f6b
mwerumuchai <mwerumuchai@gmail.com>
2017-11-26 11:13:43 +0300

    add paths to posts and edit profile

--- gram/urls.py
+++ gram/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'^$', views.homepage,name='home'),
-    url(r'^profiles/', views.update_profile,name='profile'),
+    url(r'^profiles/$', views.profile,name='profiles'),
+    url(r'^profiles/post', views.posts,name='uploadpost'),
+    url(r'^profiles/edit', views.update_profile,name='editprofile'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
