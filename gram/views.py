2074a66205e30ce6f39e597574e8138fc284562d
mwerumuchai <mwerumuchai@gmail.com>
2017-11-26 20:08:47 +0300

    add redirect

--- gram/views.py
+++ gram/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib import messages
