10c67a3137b93f2618f77bdab64b5715f1451a53
mwerumuchai <mwerumuchai@gmail.com>
2017-11-26 11:12:02 +0300

    add Posts and COmments in the models file

--- gram/models.py
+++ gram/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
+import datetime as dt
 # genders = (
 #     ('male','Male'),
 #     ('female' 'Female'),
@@ -17,13 +18,41 @@ class Profile(models.Model):
     phone_number = PhoneNumberField(max_length=10, blank=True)
     # gender = models.CharField(max_length=25,choices=genders)
     location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    birth_date = models.DateTimeField(null=True, blank=True)
 
-@receiver(post_save, sender=User) #post_save:signal for whenever save event occurs
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User) #post_save:signal for whenever save event occur
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+class Posts(models.Model):
+    image = models.ImageField(upload_to = 'photos/', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    post_date = models.DateTimeField(auto_now_add = True)
+    description = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ['-post_date']
+
+    @classmethod
+    def display_post(cls):
+        post = Posts.objects.all()
+        return post
+
+
+class Comments(models.Model):
+    comment = models.TextField(max_length=255, blank=True)
+    posts = models.ForeignKey(Posts)
+    user = models.ForeignKey(User)
+    pub_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.comment
