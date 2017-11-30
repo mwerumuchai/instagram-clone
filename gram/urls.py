3e1e66a3b3b0b2c7d5f1ebfd614d9fcb0d079a19
mwerumuchai <mwerumuchai@gmail.com>
2017-11-30 11:49:50 +0300

    In each url add the routes for profiles, posts and like functionalities

--- gram/urls.py
+++ gram/urls.py
@@ -6,9 +6,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'^$', views.homepage,name='home'),
-    url(r'^profiles/$', views.profile,name='profiles'),
-    url(r'^profiles/post', views.posts,name='uploadpost'),
-    url(r'^profiles/edit', views.update_profile,name='editprofile'),
+    url(r'^profiles/(?P<username>[-_\w.]+)$', views.profile,name='profiles'),
+    url(r'^profiles/post/(?P<username>[-_\w.]+)$', views.posts,name='uploadpost'),
+    url(r'^profiles/edit/(?P<username>[-_\w.]+)$', views.update_profile,name='editprofile'),
+    url(r'^update-profile-picture/(?P<username>[-_\w.]+)$', views.profile_pic_update, name='update_profilepic'),
+    url(r'^(?P<slug>[\w\-]{10})/like/$',views.like_post_view,name='like'),
+    url(r'^(?P<slug>[\w\-]{10})/unlike/$',views.unlike_post_view,name='unlike'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
